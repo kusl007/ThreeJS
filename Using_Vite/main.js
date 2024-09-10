9a9886d4518f4ad96e11747698561f289478e55d
Kushal Bansal <kushal.bansal.1001@gmail.com>
2024-09-11 01:32:47 +0545

    Three JS  with vite added

---
+++ Using_Vite/main.js
@@ -0,0 +1,64 @@
+import * as THREE from 'three';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+
+const canvas = document.querySelector('#canvas');
+
+// 1.Scene
+const scene = new THREE.Scene();
+scene.background = new THREE.Color("f0f0f0");
+
+// 2. Camera
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+camera.position.z = 5;
+// scene.add(camera);  
+
+// 3. Object
+
+const geometry = new THREE.DodecahedronGeometry();
+const material = new THREE.MeshLambertMaterial({ color: "#458585", emissive: "#458585" });
+const dodecahedron = new THREE.Mesh(geometry, material);
+
+const boxGemetry = new THREE.BoxGeometry(2,0.1,2);
+const boxMaterial = new THREE.MeshStandardMaterial({ color: "#b4b4b3",emissive: "#b4b4b3" });
+const box = new THREE.Mesh(boxGemetry, boxMaterial);
+box.position.y = -1.5;
+
+scene.add(dodecahedron);
+scene.add(box);
+
+
+// 4. Light
+const light = new THREE.SpotLight(0x006769, 100);
+light.position.set(1, 1, 1);
+scene.add(light);
+
+// 5. Renderer
+const renderer = new THREE.WebGLRenderer({ canvas });
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setPixelRatio(window.devicePixelRatio);
+
+// 6.Add orbital controls
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.enableDamping = true;
+controls.dampingFactor = 0.05;
+controls.enableZoom = true;
+controls.enablePan= true;
+
+//7. Animate
+function animate() {
+  requestAnimationFrame(animate);
+  dodecahedron.rotation.x += 0.01;
+  dodecahedron.rotation.y += 0.01;
+  box.rotation.y += 0.005;
+  controls.update();
+  renderer.render(scene, camera);
+}
+
+//8. handle window resizing
+window.addEventListener('resize', () => {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  // renderer.setPixelRatio(window.devicePixelRatio);
+})
+animate()
